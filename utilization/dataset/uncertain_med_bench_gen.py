2c9c5908102e7a6a4c42c1cded562d5c6249352c
xansar <xxzhang16@fudan.edu.cn>
2024-08-01 13:55:09 +0800

    fix reset bug

--- utilization/dataset/uncertain_med_bench_gen.py
+++ utilization/dataset/uncertain_med_bench_gen.py
@@ -2,12 +2,12 @@ from functools import cached_property
 from logging import getLogger
 
 from .generation_dataset import GenerationDataset
-from .dataset_utils import load_raw_dataset_from_file, get_raw_dataset_loader
 from datasets import load_dataset
-from ..metric import Rouge, Bleu, BERTScore
+from ..metric import Rouge, BERTScore
 import os.path as osp
-import re
-from ..dataset_enum import UNCERTAIN_MED_BENCH_GEN_SUBJECTS
+import random
+# from ..metric import ECE, AUPRC, AUROC
+
 logger = getLogger(__name__)
 
 class UncertainMedBenchGen(GenerationDataset):
@@ -19,7 +19,8 @@ class UncertainMedBenchGen(GenerationDataset):
     }
     instruction = None
     evaluation_set = "test"
-    metrics = [Rouge(), Bleu(), BERTScore()]
+    metrics = [Rouge(), BERTScore()]
+    # calib_metrics = [ECE(), AUROC(), AUPRC()]
     load_args = ('uncertain_med/uncertain_med_bench_gen', 'rjua_treatment', 'huatuo_encyclopedia', 'qizhen_drug', 'cmb_clin')
     example_set = None
     # load_args = ("/data/home/xiangxu_zhang/codes_repo/HealthLLM/benchmark/CMExam/data",)
@@ -42,19 +43,54 @@ class UncertainMedBenchGen(GenerationDataset):
         results, score_lists = super().calculate_metric(predictions)
         return results, score_lists
 
+    def data_sample_with_threshold(self, subset_name, evaluation_data, local_rng, threshold=200):
+        if len(evaluation_data) < threshold:
+            logger.debug(f"{subset_name} indices: {[0, 1, 2, 3, 4]}")
+            return evaluation_data
+        else:
+            sample_size = threshold
+            # 使用随机抽样的方法从数据集中抽取样本
+            # 设置局部种子
+            sampled_indices = local_rng.sample(range(len(evaluation_data)), sample_size)
+            logger.debug(f"{subset_name} indices: {sampled_indices[:5]}")
+            # print(f"{subset_name} indices: {sampled_indices[:5]}")
+            sampled_dataset = evaluation_data.select(sampled_indices)
+            return sampled_dataset
+        
     @cached_property
     def references(self):
         # print([ord(instance["Answer"]) - ord('A') for instance in self.evaluation_data])
         return [instance["answer"] for instance in self.evaluation_data]
     
+    # def load_raw_dataset(self, dataset_path, subset_name, evaluation_set, example_set):
+    #     if subset_name is None:
+    #         subset_name = self.load_args[1]
+    #     self._set_instruction(subset_name)
+    #     self.evaluation_data = load_dataset(
+    #         dataset_path,
+    #         subset_name,
+    #         split=evaluation_set,
+    #         cache_dir=osp.join(dataset_path, '.cache'),
+    #     )
+    
     def load_raw_dataset(self, dataset_path, subset_name, evaluation_set, example_set):
-        if subset_name is None:
-            subset_name = self.load_args[1]
         self._set_instruction(subset_name)
-        self.evaluation_data = load_dataset(
+        evaluation_data = load_dataset(
             dataset_path,
             subset_name,
             split=evaluation_set,
             cache_dir=osp.join(dataset_path, '.cache'),
         )
+        local_rng = random.Random(subset_name)
+        # TODO(xansar):增加threshold 参数
+        self.evaluation_data = self.data_sample_with_threshold(subset_name, evaluation_data, local_rng)
+    
             
+    @cached_property
+    def questions(self):
+        return [instance["question"] for instance in self.evaluation_data]
+
+    @cached_property
+    def options(self):
+        return ['No options' for instance in self.evaluation_data]
+         
